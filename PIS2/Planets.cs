e52eb0ecd1efe25b38c1c6e1e2151c0a4e298488
Султан <a@a.a>
2024-10-22 23:15:23 +0700

    Добавьте файлы проекта.

---
+++ PIS2/Planets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS2
+{
+    internal class Planets
+    {
+        public string Name { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public double Radius { get; set; }
+
+        public Planets(string[] items)
+        {
+            string name = "";
+            DateTime openingDate = Convert.ToDateTime(items[items.Length - 2]);
+            double radius = Convert.ToDouble(items[items.Length - 1]);
+            Name = name;
+            OpeningDate = openingDate;
+            Radius = radius;
+
+        }
+    }
+}
---
+++ PIS2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название класса и его свойства");
+
+
+            //string[] inputItems = Console.ReadLine().Split(' '); //для ввода через консоль
+
+            string[] inputItems = test().Split(' '); // функция с вводными данными
+
+            for (int i = 0; i < inputItems.Count(); i++)
+            {
+                inputItems[i] = inputItems[i].Replace("\"", "");
+            }
+
+            if (inputItems[0] == "Planets")
+            {
+                Planets planet = new Planets(inputItems);
+
+                for (int x = 1; x < inputItems.Length - 2; x++)
+                {
+                    planet.Name += inputItems[x] + " ";
+                }
+                Console.WriteLine(planet.Name);
+                Console.WriteLine(planet.OpeningDate);
+                Console.WriteLine(planet.Radius);
+            }
+
+        }
+        public static string test()
+        {
+            string input = "Planets \"Q W E   R TY\" 2024.01.01 13,1435";
+            return input;
+
+        }
+
+    }
+}
